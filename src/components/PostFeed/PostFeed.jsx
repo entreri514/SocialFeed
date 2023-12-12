4163252f6f876611d3e380aaf4cfe1c9f9c9c039
entreri514 <148565529+entreri514@users.noreply.github.com>
2023-12-12 13:16:50 -0600

    Added dummy postfeed; names

--- src/components/PostFeed/PostFeed.jsx
+++ src/components/PostFeed/PostFeed.jsx
@@ -1,5 +1,21 @@
+import App from "../../App";
+import PostInputField from "../PostInputField/PostInputField";
 const PostFeed = ({}) => {
-  return <div></div>;
+  var posts = [
+    "postfeedpostfeedpostfeedpostfeed",
+    "postfeedpostfeedpostfeedpostfeed",
+    "postfeedpostfeedpostfeedpostfeed",
+  ];
+  var postItems = posts.map((post) => <div>{post}</div>);
+  var likeDislike;
+  var names = ["Julius Caesar", "Cassius", "Brutus"];
+  var nameItems = names.map((name) => <div>{name}</div>);
+  return (
+    <div>
+      {nameItems}
+      {postItems}
+    </div>
+  );
 };
 
 export default PostFeed;
--- src/components/PostInputField/PostInputField.jsx
+++ src/components/PostInputField/PostInputField.jsx
@@ -1,3 +1,5 @@
+import App from "../../App";
+
 const PostInputField = ({}) => {
   var inputName = "";
   var inputPost = "";
