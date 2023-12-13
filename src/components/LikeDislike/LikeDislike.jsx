b385049dc24753ea1a7d3366a695a6b5acdf0990
entreri514 <148565529+entreri514@users.noreply.github.com>
2023-12-13 15:42:48 -0600

    Completed LikeDislike component

--- src/components/LikeDislike/LikeDislike.jsx
+++ src/components/LikeDislike/LikeDislike.jsx
@@ -1,12 +1,26 @@
 import PostFeed from "../PostFeed/PostFeed";
+import React, { useState } from "react";
+import "./LikeDislike.css";
 const LikeDislike = ({}) => {
-  const clickLikeDislike = (e) => {
-    console.log(e);
+  const [isLike, setIsLike] = useState(false);
+  const [isDislike, setIsDislike] = useState(false);
+  console.log(isLike);
+  const clickLike = (e) => {
+    if (isDislike === false) setIsLike(!isLike);
   };
+  const clickDislike = (e) => {
+    if (isLike === false) setIsDislike(!isDislike);
+  };
+  const btnClass = isLike ? "active-btn" : "";
+  const btnClass2 = isDislike ? "active-btn2" : "";
   return (
     <div className="input-item">
-      <button onClick={clickLikeDislike}>Like</button>
-      <button onClick={clickLikeDislike}>Dislike</button>
+      <button className={btnClass} onClick={clickLike}>
+        Like
+      </button>
+      <button className={btnClass2} onClick={clickDislike}>
+        Dislike
+      </button>
     </div>
   );
 };
